1c59a445d527612edc3b6a10231631aaf429e506
kimteasick <notseph8556@gmail.com>
2023-01-24 15:17:58 +0900

    added 230124

--- src/components/Main.Chart.tsx
+++ src/components/Main.Chart.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from "react";
-import { VictoryChart } from "victory-chart";
+import { Border, VictoryChart, VictoryLine } from "victory";
 
 const MainChart: FC = () => {
   return (
-    <div>
-      <VictoryChart domain={{ y: [0, 100] }} scale={{ x: "time", y: "linear" }}>
-        {/* 
-        <VictoryBar
+    <>
+      <VictoryChart
+        domain={{ y: [0, 10] }}
+        scale={{ x: "time", y: "linear" }}
+        style={{ parent: { backgroundColor: "#202222" } }}
+      >
+        <VictoryLine
           style={{
-            data: { stroke: "#c43a31" },
-            parent: { border: "1px solid #ccc" },
+            data: { stroke: "#45A452" },
+            parent: { border: "1px solid #fff" },
           }}
           data={[
             { x: 1, y: 2 },
@@ -17,11 +20,17 @@ const MainChart: FC = () => {
             { x: 3, y: 5 },
             { x: 4, y: 4 },
             { x: 5, y: 7 },
+            { x: 6, y: 6 },
+            { x: 7, y: 3 },
+            { x: 8, y: 4 },
+            { x: 9, y: 6 },
+            { x: 10, y: 5 },
+            { x: 11, y: 2 },
+            { x: 12, y: 7 },
           ]}
         />
-      */}
       </VictoryChart>
-    </div>
+    </>
   );
 };
 
--- src/components/Main.KospiKosdaq.tsx
+++ src/components/Main.KospiKosdaq.tsx
@@ -1,27 +1,21 @@
 import React, { FC } from "react";
 import { NextPage } from "next";
-import { VictoryBar } from "victory";
-
-const temp = [
-  { quarter: 1, earnings: 13000 },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 },
-] as const;
-// import MainChart from "./Main.Chart";
+import MainChart from "./Main.Chart";
 import "./Main.css";
 
 interface KoKoProps {
   title: string;
 }
 
-const KoKo: FC<KoKoProps> = ({ title }) => {
+const KoKo: NextPage<KoKoProps> = ({ title }) => {
   return (
     <div className="koko">
       <div className="koko_title">{title}</div>
-      <div>
-        {/* <MainChart /> */}
-        <VictoryBar data={temp} x="quarter" y="earnings" />
+      <div className="wrapper_section">
+        <div className="value_section">2400</div>
+        <div className="chart_section">
+          <MainChart />
+        </div>
       </div>
     </div>
   );
